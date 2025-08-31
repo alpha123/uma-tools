20302d0322d5e9a7be4371c5389d0b0551ada1ff
chotomatte <yuhong270991@gmail.com>
2025-08-31 23:08:47 +0800

    Update app.tsx
    update gemini cm as default

--- umalator/app.tsx
+++ umalator/app.tsx
@@ -33,7 +33,7 @@ function skillmeta(id: string) {
 
 import './app.css';
 
-const DEFAULT_COURSE_ID = CC_GLOBAL ? 10606 : 10807;
+const DEFAULT_COURSE_ID = CC_GLOBAL ? 10811 : 10807;
 const DEFAULT_SAMPLES = 500;
 const DEFAULT_SEED = 2615953739;
 
