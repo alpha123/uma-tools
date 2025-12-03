b6de948177cd266ea28cc2c1dd1d86ce11966e37
pecan <turkchess123@gmail.com>
2025-12-02 20:29:37 -0700

    no longer need a separate make_skill_data.pl for global
    global has the precondition columns now

--- umalator-global/update.bat
+++ umalator-global/update.bat
@@ -6,7 +6,7 @@ if "%1" == "" (
   set mastermdb=%1
 )
 
-perl make_global_skill_data.pl %mastermdb% > skill_data.json
+perl ../uma-skill-tools/tools/make_skill_data.pl %mastermdb% > skill_data.json
 perl make_global_skillnames.pl %mastermdb% > skillnames.json
 perl make_global_skill_meta.pl %mastermdb% > skill_meta.json
 perl make_global_uma_info.pl %mastermdb%
