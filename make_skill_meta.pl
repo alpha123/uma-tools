93c3bb3d92df4d4b6f7bd00b2a1509e137e43188
pecan <turkchess123@gmail.com>
2026-05-06 14:04:48 -0600

    guess iconId for new skills where it is 0 in master.mdb
    for some reason the skill_data.icon_id column is 0 for recent skills. guess based on the ability type of their first ability. a bit brittle.

--- make_skill_meta.pl
+++ make_skill_meta.pl
@@ -12,6 +12,30 @@ if (!@ARGV) {
 
 my $mastermdb = shift @ARGV;
 
+sub clamp_rarity {
+	my ($rarity) = @_;
+	if ($rarity >= 3 && $rarity <= 5) {
+		return 3;
+	} else {
+		return $rarity;
+	}
+}
+
+sub icon_base_for_type {
+	my ($id, $type, $modifier) = @_;
+	die "probable scenario evolution with custom icon id (skill id $id)" if substr($id, 0, 1) == '4';
+	die "negative ability modifier probably indicates a debuff skill; icon not detected (skill id $id)" if $modifier < 0;
+	if ($type == 27 || $type == 22) {  # target speed / current speed
+		return '2001';
+	} elsif ($type == 9) {  # recovery
+		return '2002';
+	} elsif ($type == 31) {
+		return '2004';
+	} else {
+		die "unknown icon: unhandled ability type $type for skill id $id";
+	}
+}
+
 my $db = DBI->connect("dbi:SQLite:$mastermdb", undef, undef, {
 	sqlite_open_flags => SQLITE_OPEN_READONLY
 });
@@ -19,7 +43,7 @@ $db->{RaiseError} = 1;
 
 my $select = $db->prepare(<<SQL
    SELECT s.id, COALESCE(s3.group_id, s2.group_id, s.group_id), s.icon_id, COALESCE(sp.need_skill_point,0),
-          s.grade_value, s.disp_order
+          s.grade_value, s.disp_order, s.rarity, s.ability_type_1_1, s.float_ability_value_1_1
      FROM skill_data s
 LEFT JOIN single_mode_skill_need_point sp
        ON s.id = sp.id
@@ -37,7 +61,7 @@ SQL
 
 $select->execute;
 
-$select->bind_columns(\my ($id, $group_id, $icon_id, $sp_cost, $grade_value, $disp_order));
+$select->bind_columns(\my ($id, $group_id, $icon_id, $sp_cost, $grade_value, $disp_order, $rarity, $ability_type, $modifier));
 
 my $skills = {};
 while ($select->fetch) {
@@ -45,6 +69,9 @@ while ($select->fetch) {
 		$group_id += 9000;
 	} elsif ($group_id >= 9000000) {  # put evolved unique inherits in the same group as the unevolved version
 		$group_id = 90000 + $group_id % 10000;
+	}
+	if ($icon_id == 0) {
+		$icon_id = icon_base_for_type($id, $ability_type, $modifier) . clamp_rarity($rarity);
 	}
 	$skills->{$id} = {
 		groupId => "$group_id",
